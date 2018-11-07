6946f97b8a2346e47ce849715f48a0830db66d00
Sergei Kundriukov <Sergei_Kundriukov@epam.com>
2018-11-07 13:41:42 +0300

    Add "back to normal" button

--- src/components/Server/AppServersDetail.vue
+++ src/components/Server/AppServersDetail.vue
@@ -7,6 +7,7 @@
     <template v-else>
       <h3>Server ID: #{{ selectedServerData.id }}</h3>
       <p>Server status: {{ selectedServerData.status }}</p>
+      <button @click="backToNormal">Back to Normal</button>
     </template>
   </div>
 </template>
@@ -22,19 +23,36 @@
     },
     computed: {
       selectedServerData() {
-        const selectedServer = this.serversList.find(server => (
-          server.id === this.selectedServerId
-        ));
+        const selectedServer = this.findCurrentServer();
 
         if (selectedServer === undefined) {
-          return {
-            id: this.selectedServerId,
-            status: 'Server not found',
-          };
+          return getNotFoundServer();
         }
 
         return selectedServer;
       },
+    },
+    methods: {
+      backToNormal() {
+        const selectedServer = this.findCurrentServer()
+        
+        if (selectedServer === undefined) {
+          return getNotFoundServer();
+        }
+        
+        selectedServer.status = 'Normal';
+      },
+      findCurrentServer() {
+        return this.serversList.find(server => (
+          server.id === this.selectedServerId
+        ));
+      },
+      getNotFoundServer() {
+        return {
+          id: this.selectedServerId,
+          status: 'Server not found',
+        };
+      }
     }
   };
 </script>
